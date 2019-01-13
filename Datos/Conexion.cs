53335b598da323d78053f9c68704d8d95c61292d
samirpazo <32174672+samirpazo@users.noreply.github.com>
2019-01-12 20:13:50 -0500

    Metodos

---
+++ Datos/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        public string GetCadena()
+        {
+            ConnectionStringSettings config = ConfigurationManager.ConnectionStrings["micadena"];
+            return config.ConnectionString;
+        }
+    }
+}
---
+++ Recursos/REncriptar.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursos
+{
+    public class REncriptar
+    {
+        public string ObtenerMd5Hash(MD5 md5Hash, string clave)
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(clave));
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+        
+        public bool VerificarMd5Hash(MD5 md5Hash, string clave, string hash)
+        {
+            string hashOfInput = ObtenerMd5Hash(md5Hash, clave);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /*string source = "Hello World!";
+            using (MD5 md5Hash = MD5.Create())
+            {
+                string hash = GetMd5Hash(md5Hash, source);
+
+                Console.WriteLine("The MD5 hash of " + source + " is: " + hash + ".");
+
+                Console.WriteLine("Verifying the hash...");
+
+                if (VerifyMd5Hash(md5Hash, source, hash))
+                {
+                    Console.WriteLine("The hashes are the same.");
+                }
+                else
+                {
+                    Console.WriteLine("The hashes are not same.");
+                }
+            }*/
+    }
+}
